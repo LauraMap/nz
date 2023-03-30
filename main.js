02e34c15f32988d2178d08478bb75e7ce947d673
LauraMap <laura.saxer@student.uibk.ac.at>
2023-03-30 16:07:38 +0200

    array stops definiert und angezeigt

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ let stop_lng = 174.001944;
 let zoom = 13;
 let title = 'Picton';
 
+const STOPS = ['Abel-Tasman-Nationalpark', 'Picton', 'Wellington'];
+console.log(STOPS);
+console.log(STOPS[0]);
+console.log(STOPS[1]);
+console.log(STOPS[2]);
+console.log(STOPS.length);
+
+
+
 let map = L.map('map').setView([stop_lat, stop_lng ], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
