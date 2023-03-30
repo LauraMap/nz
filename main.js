42c46580a4a6ca11e07d2c65d8e2ec9496b52b28
LauraMap <laura.saxer@student.uibk.ac.at>
2023-03-30 15:52:50 +0200

    variablen stops zoom title erstellt

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var map = L.map('map').setView([-41.293056, 174.001944], 13);
+let stop_lat = -41.293056;
+let stop_lng = 174.001944;
+let zoom = 13;
+let title = 'Picton';
+
+let map = L.map('map').setView([stop_lat, stop_lng ], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-41.293056, 174.001944]).addTo(map)
-    .bindPopup('Picton')
+L.marker([stop_lat, stop_lng]).addTo(map)
+    .bindPopup(title)
     .openPopup();
